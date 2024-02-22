7b578ea03f8a198cabc1aeb066ed74a79a766018
Alexey Stashatov <alex008145@gmail.com>
2024-02-22 18:18:16 +0300

    fixing

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,12 @@
+import { Link } from "react-router-dom";
+
 export function Home() {
   return (
     <div>
       <h1>Home</h1>
       <p>
         Welcome to our store. You can find products in{" "}
-        <a href="./store">Store</a> and add it to your cart. Have a nice day!
+        <Link to="/store">Store</Link> and add it to your cart. Have a nice day!
       </p>
     </div>
   );
