0e437035a412c2b67b245fa8b7e5ddcb606bef9c
Alexey Stashatov <alex008145@gmail.com>
2024-02-22 18:13:10 +0300

    fixing error

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -4,7 +4,7 @@ export function Home() {
       <h1>Home</h1>
       <p>
         Welcome to our store. You can find products in{" "}
-        <a href="/store">Store</a> and add it to your cart. Have a nice day!
+        <a href="./store">Store</a> and add it to your cart. Have a nice day!
       </p>
     </div>
   );
